4f9102e404ee137a781c8580b829a340c5a5d428
rosell-dk <bjorn@rosell.dk>
2023-10-20 14:10:28 +0200

    Call simple_html_dom with forceTagsClosed = true in order not to trigger warning in PHP8.2. Fixes #43

--- src/PictureTags.php
+++ src/PictureTags.php
@@ -153,7 +153,7 @@ class PictureTags
             // It would perhaps have been better to try to guess the encoding rather than
             // changing it (see #39), but I'm reluctant to introduce changes.
             $html =  self::textToUTF8WithNonAsciiEncoded($html);
-            $dom = HtmlDomParser::str_get_html($html, false, false, 'UTF-8', false);
+            $dom = HtmlDomParser::str_get_html($html, false, true, 'UTF-8', false);
             if ($dom !== false) {
                 $elems = $dom->find('img,IMG');
                 foreach ($elems as $index => $elem) {
